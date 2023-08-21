0a2f77dcf96773bea8682c47fb55d1d7e3344de0
Sabin Thapa <112601289+Sabinkthapa@users.noreply.github.com>
2023-08-21 04:52:44 -0300

    added peudo coding for better understanding for users before coding

---
+++ script.js
@@ -0,0 +1,13 @@
+// store object questions in the array
+
+// use method getelementById to select the element from DOM.
+
+// initalias the value for like questions ,score
+
+// declare function to start the quiz
+// execute the function for timer, questions from the store object questions
+
+// add button in the store object questions
+// check for correct answer -if condition.
+
+// use local storage to store the date, use prevent display to retrict reload 
